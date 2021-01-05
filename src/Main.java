475cd9d3fce06393d43f62da25098a95123348f1
xtreme6007 <prestonnichols96@gmail.com>
2021-01-05 15:18:50 -0600

    added switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
 public class Main {
-    public static void main(String[] args){
-
-
+    public static void main(String[] args) {
 
 
     }
 
-    public static void printDayOfTheWeek(int day){
-
+    public static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+        }
 
     }
 
